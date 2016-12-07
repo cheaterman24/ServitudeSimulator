1fa2ca5f0c225b45f46e900cf9bf99a6c459bc8b
Unknown <anonymous2435@gmail.com>
2016-12-07 01:51:04 -0500

    Fixing Text box

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -4,20 +4,34 @@ using System.Collections;
 
 public class NPCController : MonoBehaviour {
 	Text name, body;
+	GameObject textBox;
 	// Use this for initialization
 	void Start () {
-		name = GameObject.FindGameObjectWithTag ("TextName");
-		body = GameObject.FindGameObjectWithTag ("TextBody");
+		name = GameObject.FindGameObjectWithTag ("TextName").GetComponent<Text> ();
+		body = GameObject.FindGameObjectWithTag ("TextBody").GetComponent<Text>();
+		textBox = GameObject.FindGameObjectWithTag ("TextBox");
+		textBox.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		textBox.transform.position = new Vector3 (transform.position.x, transform.position.y + 2f, transform.position.z);
 	
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.tag == "Player") {
 			//initiate Dialog
+			textBox.SetActive(true);
+			name.text = "TEST NPC";
+			body.text = "Hello, How are you?";
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.tag == "Player") {
+			textBox.SetActive (false);
 		}
 	}
 }
