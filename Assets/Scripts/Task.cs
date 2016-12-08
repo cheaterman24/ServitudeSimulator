c435a12dae68cd6da7f838205a0e02c2ef603d32
Unknown <anonymous2435@gmail.com>
2016-12-08 15:49:23 -0500

    Working on Task Managing

--- Assets/Scripts/Task.cs
+++ Assets/Scripts/Task.cs
@@ -1,15 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class Task : MonoBehaviour {
+public class Task : IComparable<Task>{
 
-	// Use this for initialization
-	void Start () {
-	
+	public string description; // description of what the player must do to complete the task
+	public string name; // name of the task
+	public string type; // type of task, gathering, speaking, action, etc.
+	public bool main; // determines if quest is main, or secondary
+
+	public Task(string n, string d, string t)
+	{
+		name = n;
+		description = d;
+		type = t;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public int CompareTo(Task t)
+	{
+		if (t == null) {
+			return 1;
+		}
+
+		if (name == t.name) {
+			return 0;
+		} else
+			return 1;
+	}
+
+	public string Description {
+		get { return description; }
+		private set { description = value; }
+	}
+
+	public string Name {
+		get { return name; }
+		private set { name = value; }
+	}
+
+	public string Type {
+		get { return type; }
+		private set { type = value; }
+	}
+
+	public void Progress (){
+		switch (type) {
+
+		case "gather":
+			{
+				//collect some amount of...
+			}
+			break;
+		case "talk":
+			{
+				//speak to...
+			}
+			break;
+		case "give":
+			{
+				//give something to...
+			}
+			break;
+		}
+
 	}
 }
--- Assets/Scripts/TaskManager.cs
+++ Assets/Scripts/TaskManager.cs
@@ -1,8 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TaskManager : MonoBehaviour {
 
+	public List<Task> objectives;
+	public List<GameObject> bag;
+	public List<GameObject> cottonBag;
+	int maxBagSize = 10;
+
+
+	public void PickUp(ref GameObject o)
+	{
+		if (bag.Count == maxBagSize) {
+			//display bag is full message
+			//do not destroy gameobject
+		} else {
+			//might fuck up with referencing instead of copy
+			bag.Add (o);
+			Destroy (o);
+		}
+	}
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +28,6 @@ public class TaskManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
